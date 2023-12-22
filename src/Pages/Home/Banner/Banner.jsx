54f82fcf1c775493599a342ed95fe1ff5724171c
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2023-12-22 07:16:56 +0600

    create banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,16 +1,22 @@
 import banner from "../../../assets/todoBanner.jpg"
 const Banner = () => {
+    const myStyle = {
+        backgroundImage: `url(${banner})`,
+        backgroundColor: "rgba(0,0,0,0.15)",
+        backgroundPosition: "center",
+    }
     return (
-        <div className="hero min-h-screen text-white" style={{ backgroundImage: `url(${banner})` }}>
-            <div className="hero-overlay"></div>
-            <div className="hero-content text-center">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Organize your work and life, finally.</h1>
-                    <p className="mb-5">Become focused, organized, and calm with Todoist. The world’s #1 task manager and to-do list app.</p>
-                    <button className="btn btn-primary">Let’s Explore</button>
+        <>
+        <div className=" bg-cover h-100vh" style={myStyle} >
+            <div className="h-[100vh] text-center" style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+                <div className="pl-8 ml:pl-0 max-w-[1500px] mx-auto text-white pt-20 md:pt-[200px] font-kanit">
+                    <h1 className="mb-5 text-3xl md:text-7xl font-bold">Organize your work <br /> and life, <span className="text-[#e76f51]">finally.</span></h1>
+                    <p className="mb-10 md:text-xl">Become focused, organized, and calm with Todoist. <br /> The world’s #1 task manager and to-do list app.</p>
+                   <button className="p-3 rounded-lg hover:bg-[#F7B787] btn-outline transition-all hover:text-black bg-[#e76f51] text-white">Let’s Explore</button>
                 </div>
             </div>
         </div>
+    </>
     );
 };
 
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,38 +1,70 @@
-import Container from "../../Components/Container";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+    const navLinks = <>
+        <li>
+            <NavLink to="/" className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-[#e76f51]" : ""}>
+                Home
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/petLesting" className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-[#e76f51]" : ""}>
+                Pet Lesting
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/donationcampaign" className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-[#e76f51]" : ""}>Donation Campaigns
+            </NavLink>
+        </li>
+        <li>
+            {/* <NavLink to="/login" className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "text-[#e76f51]" : ""}>
+                    Login
+            </NavLink> */}
+
+            {/* {
+                user ?
+                    ''
+                    :
+                    <Link to="/login">
+                        <button className="btn">Login</button></Link>
+            } */}
+        </li>
+    </>
     return (
-            <div className="drawer border-b-2 max-w-[1600px] mx-auto md:px-10">
-                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col">
-                    {/* Navbar */}
-                    <div className="w-full navbar">
-                        <div className="flex-none md:hidden">
-                            <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                            </label>
-                        </div>
-                        <div className="flex-1 px-2 mx-2 text-2xl font-bold">ProjectPulse</div>
-                        <div className="flex-none hidden md:block">
-                            <ul className="gap-6 menu-horizontal">
-                                {/* Navbar menu content here */}
-                                <li><a>Navbar Item 1</a></li>
-                                <li><a>Navbar Item 2</a></li>
-                            </ul>
-                        </div>
+        <div className="drawer md:px-10 fixed bg-black z-10 bg-opacity-10 text-white">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="w-full navbar max-w-[1600px] mx-auto">
+                    <div className="flex-none md:hidden">
+                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
                     </div>
-                    {/* Page content here */}
-                    
+                    <div className="flex-1 mx-2 text-2xl font-bold"><span className="text-[#e76f51]">Project</span>Pulse</div>
+                    <div className="flex-none hidden md:block">
+                        <ul className="gap-6 menu-horizontal font-semibold text-lg">
+                            {/* Navbar menu content here */}
+                           {navLinks}
+                        </ul>
+                    </div>
                 </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200">
-                        {/* Sidebar content here */}
-                        <li><a>Sidebar Item 1</a></li>
-                        <li><a>Sidebar Item 2</a></li>
-                    </ul>
-                </div>
+                {/* Page content here */}
+
             </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                    {/* Sidebar content here */}
+                    <li><a>Home</a></li>
+                    <li><a>About</a></li>
+                </ul>
+            </div>
+        </div>
     );
 };
 
